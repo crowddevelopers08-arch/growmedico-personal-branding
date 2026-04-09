4837834f0dd49207002b91b013b4d9f7f9694f9b
fenil matt fenil matt <ffenilmatt@gmail.com>
2026-04-09 12:06:42 +0530

    kkss

--- components/landing/config.ts
+++ components/landing/config.ts
@@ -1,1 +1,2 @@
-export const CALENDLY_URL = "https://calendly.com/d/cyjf-c4w-vnb";
+export const CALENDLY_URL =
+  "https://calendly.com/growmedico-gm/personal-branding-meetings-tn?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=0e8e74";
